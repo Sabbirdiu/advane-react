3a6b0cee8333bce80ff254b5cb19448ee11ef829
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-05-31 13:15:55 +0600

    add multiple state values

--- src/hooks/UseStateObject.js
+++ src/hooks/UseStateObject.js
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
 
 const UseStateObject = () => {
-  const [person, setPerson] = useState({
-    name: "sabbir",
-    age: 25,
-    message: "industry is not everything",
-  });
+  const [name, setName] = useState("sabbir");
+  const [age, setAge] = useState(25);
+  const [message, setMessage] = useState(
+    "initialState industry is not everything"
+  );
   const changeMessage = () => {
-    setPerson({ ...person, message: "hello message" });
+    // setPerson({ ...person, message: "hello message" });
+    setMessage("Change the message");
   };
   return (
     <>
-      <h3>{person.name}</h3>
-      <h3>{person.age}</h3>
-      <h3>{person.message}</h3>
-      <button onClick={changeMessage}> change msg </button>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button onClick={changeMessage} className="btn">
+        {" "}
+        change msg{" "}
+      </button>
     </>
   );
 };
