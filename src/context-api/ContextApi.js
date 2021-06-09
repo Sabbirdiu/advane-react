dac4ec3f3b9338e602953c5ccdc53d51a21db044
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-06-09 21:33:46 +0600

    added context api use context

---
+++ src/context-api/ContextApi.js
@@ -0,0 +1,40 @@
+import React, { useState, useContext } from "react";
+import { data } from "../data";
+const PersonContext = createContext();
+const ContextApi = () => {
+  const [people, setPeople] = useState(data);
+  const removeItem = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    });
+  };
+  return (
+    <PersonContext.Provider value={{ removeItem, people }}>
+      <h3>Context Api</h3>
+      <List />
+    </PersonContext.Provider>
+  );
+};
+
+const List = () => {
+  const mainData = useContext(PersonContext);
+  console.log(mainData);
+  return (
+    <>
+      {mainData.people.map((person) => {
+        return <SinglePerson key={person.id} />;
+      })}
+    </>
+  );
+};
+const SinglePerson = ({ id, name }) => {
+  const { removeItem } = useContext(PersonContext);
+  return (
+    <div className="item">
+      <h4>{name}</h4>
+      <button onClick={() => removeItem(id)}>remove person</button>
+    </div>
+  );
+};
+
+export default ContextApi;
